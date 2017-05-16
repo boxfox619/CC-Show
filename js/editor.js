1ebe14ec0c57b383a422217ec5f773595a512d95
boxfox <rlatjdfo112@naver.com>
2017-05-16 15:26:26 +0900

    Drag Component Complete

--- js/editor.js
+++ js/editor.js
@@ -3,6 +3,8 @@ var headerTitle = $('.mdl-layout-title.header-title');
 
 $(function(){
   replacement.hide();
+  initializeDragMouseEvent();
+  initializeDragableComponent($('.move_control'));
 });
 
 var focusout = function(){
@@ -25,3 +27,32 @@ var headerClick = function(){
 
 headerTitle.on('click', headerClick);
 headerTitle.css('cursor', 'pointer');
+
+
+/* ---------- drag & drop layout -----------*/
+var controlComponent;
+
+function initializeDragMouseEvent(){
+  $('main').mousemove(function( event ) {
+    if(controlComponent!=null&&controlComponent!=undefined){
+      var rect = $('main')[0].getBoundingClientRect();
+      var width = controlComponent.width();
+      var height = controlComponent.height();
+      var x = event.pageX-width+12;
+      var y = event.pageY-rect.top-12;
+      controlComponent.css({top: y, left: x, position:'absolute'});
+    }
+  });
+  $('main').mouseleave(function(){
+    controlComponent = null;
+  });
+}
+
+function initializeDragableComponent(component){
+  component.mouseup(function() {
+    controlComponent = null;
+  });
+  component.mousedown(function() {
+    controlComponent = component.closest('.dragable-component');
+  });
+}
