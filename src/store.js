62d22c92aa3583446ced0970c24215b787107c61
rlatjdfo112 <rlatjdfo112@naver.com>
2017-08-06 22:57:54 +0900

    Develop the store module

---
+++ src/store.js
@@ -0,0 +1,21 @@
+import { reducers } from './reducers'
+import { createStore } as Redux from 'redux'
+
+const reduxStore = createStore(reducers);
+const subscribe: (callback) =>{
+  return reduxStore.subscribe(()=>{
+    callback();
+  });
+};
+
+const subscribe: (select,callback) =>{
+  let prevState = select(reduxStore.getState());
+  return reduxStore.subscribe(()=>{
+    if(prevState != select(reduxStore.getState()){
+      callback(select(reduxStore.getState()));
+    }
+  });
+};
+
+exports.subscribe subscribe;
+exports.default reduxStore;
