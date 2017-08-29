4c0f337d890eb62b1b35f3d850e51004a955c9ff
seoyunho <yunho101775@gmail.com>
2017-08-29 14:49:00 +0900

    silde show render() 수정

--- src/components/editor/silde_show/SildeShow.js
+++ src/components/editor/silde_show/SildeShow.js
@@ -8,38 +8,40 @@ class SildeShow extends React.Component{
     }
 
     render(){
-        <div>
-            <div id={styles.silde_contents}>
-                <div>
-                    <img src="images/ic_arrow_left_big.png"/>
-                </div>
+        return(
+            <div>
+                <div id={styles.silde_contents}>
+                    <div>
+                        <img src="images/ic_arrow_left_big.png"/>
+                    </div>
                 <div>
 
                 </div>
                 <div>
                     <img src="images/ic_arrow_right_big.png"/>
                 </div>
-            </div>
-            <div id={styles.silde_sub_contents}>
-                <div id={styles.silde_show_note}>
-                    <input type="text" id={styles.silde_note_contents} placeholder="쇼 노트를 입력하세요"/>
                 </div>
-                <hr/>   
-                <div id={styles.silde_number_wrapper}>
-                    <div id={styles.silde_number_contents}>
-                        <img src="images/ic_arrow_left_small.png"/>
-                        <input type="text"/>
-                        <p>/</p>
-                        <input type="text"/>
-                        <img src="images/ic_arrow_left_small.png"/>
+                <div id={styles.silde_sub_contents}>
+                    <div id={styles.silde_show_note}>
+                        <input type="text" id={styles.silde_note_contents} placeholder="쇼 노트를 입력하세요"/>
                     </div>
-                    <div id={styles.silde_option_button}>
-                        <img src="images/ic_fullscreen.png"/>
-                        <img src="images/ic_silde_show.png"/>
+                    <hr/>   
+                    <div id={styles.silde_number_wrapper}>
+                        <div id={styles.silde_number_contents}>
+                            <img src="images/ic_arrow_left_small.png"/>
+                            <input type="text"/>
+                            <p>/</p>
+                            <input type="text"/>
+                            <img src="images/ic_arrow_left_small.png"/>
+                        </div>
+                        <div id={styles.silde_option_button}>
+                            <img src="images/ic_fullscreen.png"/>
+                            <img src="images/ic_silde_show.png"/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        )   
     }
 }
 
