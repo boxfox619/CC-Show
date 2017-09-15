be2b09c48c48706cb4879e2b5c3a122cabbc8da9
Ground-Silver0903 <kyn07077@gmail.com>
2017-09-15 16:51:09 +0900

    set imagefile

--- src/components/editor/assetEditor/assetEditorItem/AssetSetting.js
+++ src/components/editor/assetEditor/assetEditorItem/AssetSetting.js
@@ -4,24 +4,58 @@ import styles from './AssetEditorItem.css';
 class AssetSetting extends React.Component{
     constructor(prop){
         super(prop);
-
         // this.previewImage = this.previewImage.bind(this);
-        this.state = {file : ' ' , imagePreviewUrl: ''};
+        this.state = {file : ' ' , imagePreviewUrl: '', currentImageUpload: undefined};
     };
+
+    thumbNail (e){
+        this.setState({
+            currentImageUpload : 'previewThumbnail'
+        });
+    }
+    
+    inputimg(e){
+        this.setState({
+            currentImageUpload : 'previewInputimg'
+        });
+    }
+
+    inputimg(e){
+        this.setState({
+            currentImageUpload : 'previewInputimg'
+        });
+    }
 
     ImageChange(e) {
         e.preventDefault();
     
         let reader = new FileReader();
         let file = e.target.files[0];
-    
+        // let file2 = e.target.files[0];
+        // let file3 = e.target.files[0];
+
         reader.onloadend = () => {
-          this.setState({
-            file: file,
-            imagePreviewUrl: reader.result
-          });
+            switch(this.state.currentImageUpload){
+
+                case "previewThumbnail":
+                this.setState({
+                    file : file,
+                    imagePreviewUrl : reader.result
+                });
+
+                case 'previewInputimg':
+                this.setState({
+                    file : file,
+                    imagePreviewUrl : reader.result
+                });
+
+                case 'previewInputimg2':
+                this.setState({
+                    file : file,
+                    imagePreviewUrl : reader.result
+                });
+            }
         }
-    
         reader.readAsDataURL(file)
       }
 
@@ -31,20 +65,44 @@ class AssetSetting extends React.Component{
     }
     
     componentDidMount(){
-        // this.previewImage();
+        
     }
     render(){
+        
         let {imagePreviewUrl} = this.state;
         let $imagePreview = null;
-        if (imagePreviewUrl) {
-          $imagePreview = (<img src={imagePreviewUrl} accept="image/*"/>);
-        } else {
-          $imagePreview = (<div className="preview_Image">미리보기<br/>(파일을 선택하세요)</div>);
+        let $imagePreview2 = null;
+
+        switch(this.state.currentImageUpload){}
+        if(this.state.currentImageUpload === 'previewThumbnail'){
+            if(imagePreviewUrl){
+                $imagePreview = (<img src={imagePreviewUrl} accept="image/*"/>);
+            }else{
+                $imagePreview = (<div className="preview_Image">미리보기<br/>(파일을 선택하세요)</div>);
+            }
         }
+
+        if(this.state.currentImageUpload === 'previewInputimg'){
+            if(imagePreviewUrl){
+                $imagePreview2 = (<img src={imagePreviewUrl} accept="image/*"/>);
+            }else{
+                $imagePreview2 = (<div className="previewinputimg">미리보기<br/>(파일을 선택하세요)</div>);
+            }
+        }
+
+
+        
+        
+        // if (imagePreviewUrl) {
+        //   $imagePreview = (<img src={imagePreviewUrl} accept="image/*"/>);
+        // } else {
+        //   $imagePreview = (<div className="preview_Image">미리보기<br/>(파일을 선택하세요)</div>);
+        // }
+
         return(
      <div className = {styles.AssetEditor_left}>
             <div className = {styles.previewDiv}>
-                <input type = "file" className = {styles.previewFile} onChange = {(e)=>this.ImageChange(e)}/>
+                <input type = "file" className = {styles.previewFile} onClick = {(e)=>this.thumbNail(e)} onChange = {(e)=>this.ImageChange(e)}/>
                 <button className = {styles.AssetEditor_preview}>{$imagePreview}</button>
             </div>
             {/* <div className = {styles.preview_Image}>{$imagePreview}</div> */}
@@ -75,9 +133,10 @@ class AssetSetting extends React.Component{
 
                 <div className = {styles.setting_third}>
                     <div className = {styles.imagePreview}>
-                        <input type = "file" className = {styles.inputImage}/>
-                        <button className = {styles.previewText}>사진첨부</button>
+                        <input type = "file" className = {styles.inputImage} onClick = {(e)=>this.inputimg(e)} onChange = {(e)=> this.ImageChange(e)}/>
+                        <button className = {styles.previewText}>{$imagePreview2}</button>
                     </div>
+
                     <div className = {styles.imagePreview}>
                         <input type = "file" className = {styles.inputImage}/>
                         <button className = {styles.previewText}>사진첨부</button>
