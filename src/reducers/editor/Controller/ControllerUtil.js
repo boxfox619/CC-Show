63a9776717591393b1ceb6b9c16491e7bea3a88d
rlatjdfo112 <rlatjdfo112@naver.com>
2017-10-29 22:35:50 +0900

    Implement the add substyle setting function

--- src/reducers/editor/Controller/ControllerUtil.js
+++ src/reducers/editor/Controller/ControllerUtil.js
@@ -25,6 +25,60 @@ export const setSelectAssetStyle = (state, styleName, styleAttr) => {
   }
 }
 
+export const updateSelectedAssetValue = (state, value) => {
+  return {
+    ...state,
+    slides: update(
+      state.slides, {
+        [state.selectedSlide]: {
+          assets: {
+            $set: update(
+              state.slides[state.selectedSlide].assets, {
+                [state.slides[state.selectedSlide].selectedAsset]: {
+                  value: {
+                    $set: value
+                  }
+                }
+              }
+            )
+          }
+        }
+      }
+    )
+  }
+}
+
+export const replaceSelectAssetObj = (state, objName, obj)=>{
+  return {
+    ...state,
+    slides: update(
+      state.slides, {
+        [state.selectedSlide]: {
+          assets: {
+            $set: update(
+              state.slides[state.selectedSlide].assets, {
+                [state.slides[state.selectedSlide].selectedAsset]: {
+                  [objName]: {
+                    $set: obj
+                  }
+                }
+              }
+            )
+          }
+        }
+      }
+    )
+  }
+}
+
 export const getSelectedAssetStyle = (state, styleName)=>{
-  return state.slides[state.selectedSlide].assets[state.slides[state.selectedSlide].selectedAsset].style[styleName];
+  return getSelectedAsset(state).style[styleName];
+}
+
+export const getSelectedAssetValue = (state) =>{
+  return state.slides[state.selectedSlide].assets[state.slides[state.selectedSlide].selectedAsset].value;
+}
+
+export const getSelectedAsset = (state) =>{
+  return state.slides[state.selectedSlide].assets[state.slides[state.selectedSlide].selectedAsset];
 }
