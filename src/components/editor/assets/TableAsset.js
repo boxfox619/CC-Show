999bdbb21ddb1eca55899d8b41a97bc8a44e9fd2
seoyunho <yunho101775@gmail.com>
2017-10-29 22:37:44 +0900

    cell 크기 조절

--- src/components/editor/assets/TableAsset.js
+++ src/components/editor/assets/TableAsset.js
@@ -5,8 +5,47 @@ class TableAsset extends React.Component{
   constructor(props){
     super(props);
   }
+
+  componentDidUpdate(){
+    (function () {
+      var thElm;
+      var startOffset;
+      Array.prototype.forEach.call(
+        document.querySelectorAll("table td"),
+        function (th) {
+          th.style.position = 'relative';
+
+          var grip = document.createElement('div');
+          grip.innerHTML = "&nbsp;";
+          grip.style.top = 0;
+          grip.style.right = 0;
+          grip.style.bottom = 0;
+          grip.style.width = '5px';
+          grip.style.position = 'absolute';
+          grip.style.cursor = 'col-resize';
+          grip.addEventListener('mousedown', function (e) {
+            thElm = th;
+            startOffset = th.offsetWidth - e.pageX;
+          });
+
+          th.appendChild(grip);
+        });
+
+      document.addEventListener('mousemove', function (e) {
+        if (thElm) {
+          thElm.style.width = startOffset + e.pageX + 'px';
+        }
+      });
+
+      document.addEventListener('mouseup', function () {
+        thElm = undefined;
+      });
+    })();
+  }
+
   render() {
-    let styles={'border-style':'solid', ...this.props.asset.style};
+    let styles={'borderStyle':'solid', ...this.props.asset.style};
+    console.log(this.props.styles);
     let getColumns=() => {
       return(
         this.props.asset.cells.map(function(row){
@@ -23,16 +62,16 @@ class TableAsset extends React.Component{
       return(
         row.map(function(value){
           return(
-            <td contentEditable={true} onChange="" style={{'border-spacing':'0', ...styles}}>
+            <td onChange="" style={{'borderSpacing':'0', ...styles}}>
               {value}
             </td>
           )
         })
       )
     }
-
+    
     return(
-      <table style={{'border-spacing':'0', 'padding':'0', ...this.props.styles}}>
+      <table style={{'borderSpacing':'0', 'padding':'0', ...this.props.styles}}>
         <tbody>
           {getColumns()}
         </tbody>
