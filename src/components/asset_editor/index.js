07173a7d799ffac0c29f5304c7e9ea2104d4bd96
rlatjdfo112 <rlatjdfo112@naver.com>
2017-12-08 23:28:43 +0900

    Code Editor 분리

---
+++ src/components/asset_editor/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ProgressDialog from './progressDialog/ProgressDialog';
+import { bindActionCreators } from 'redux';
+import AssetEditorItem from './AssetEditorItem';
+import CodeEditorItem from './Code/CodeEditor';
+import axios from 'axios';
+
+import styles from './style.css';
+
+import axios from 'axios';
+
+
+class AssetEditor extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      activeTab: 0,
+      id: '',
+      title: '',
+      openToStore: false,
+      thumbnail: '',
+      images: [],
+      content: '',
+      price: 0,
+      license: '',
+      value : '',
+      nowMode : '',
+      code : '',
+      html : '',
+      css : '',
+      js : '',
+      mouseAction: 'none',
+      previewAsset:{
+        id: 'preview',
+        type: assetTypes.TYPE_PREVIEW,
+        value: '',
+        height: '50px',
+        width: '50px',
+        x: '0px',
+        y: '0px',
+        angle: '0',
+        style: {}
+      }
+    };
+
+    this.state = {showId: undefined};
+  }
+
+  render(){
+    return (
+      <div ref={root => {this.root = root}} >
+        <AssetEditorItem />
+      </div>
+    );
+  }
+}
+
+export default AssetEditor;
