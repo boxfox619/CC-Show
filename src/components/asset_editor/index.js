0b664138fccb8d46c7c2f9032080613d724d6846
rlatjdfo112 <rlatjdfo112@naver.com>
2017-12-10 11:50:39 +0900

    Asset Editor redesigm

--- src/components/asset_editor/index.js
+++ src/components/asset_editor/index.js
@@ -36,8 +36,8 @@ class AssetEditor extends React.Component{
     return (
       <div style={{'marginTop':headerHeight}} ref={root => {this.root = root}}>
       <div className={styles.attribute_editor}>
-      <Title title={this.state.title} onChange={this.titleHandle}/>
-      <Toggle text={'스토어에 공개'} checked={this.state.openToStore} onChange={this.openToStoreHandle}/>
+        <Title title={this.state.title} onChange={this.titleHandle}/>
+        <Toggle text={'스토어에 공개'} checked={this.state.openToStore} onChange={this.openToStoreHandle}/>
       </div>
         <CodeEditorItem/>
       </div>
