2e297c43905fb3b42403d3ccbc9d67418e9d5e71
kimsr <kimsr@satreci.com>
2017-12-28 12:53:54 +0900

    [UPDATE] seperate service logic in SlideList & Prepare for index page

---
+++ src/scenes/ShowList/services/request.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export const createShow = (name, callback) =>{
+  let result = {result: false};
+  axios.post('/show/create', {name})
+  .then(response => {
+    result['result'] = true;
+    callback(result);
+  }).catch(err => {
+    callback(result);
+  });
+}
+
+export const loadShowList = (callback) =>{
+  let result = {result: false};
+  axios.get('/show/list')
+  .then(response => {
+    result['result'] = true;
+    result['data'] = response.data;
+    callback(result);
+  }).catch(err => {
+  callback(result);
+  });
+}
+
+export const deleteShow = (callback) =>{
+  axios.post('/show/delete', {id})
+  .then(response => {
+    callback(true);
+  }).catch(err => {
+    callback(false);
+  });
+}
