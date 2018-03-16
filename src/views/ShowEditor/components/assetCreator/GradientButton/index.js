5cb583336187a0d8dcc5281e674dea1185ab4c79
Boxfox <boxfoxsg619@gmail.com>
2018-03-16 14:38:00 +0900

    Remove legacy code from AssetCreator Component

---
+++ src/views/ShowEditor/components/assetCreator/GradientButton/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './style.css';
+
+const propTypes = {
+    label: React.PropTypes.string.isRequired
+}
+
+class GradientButton extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={styles.clickableButton} onClick={this.props.onClick}>
+                <div>
+                    {this.props.label}
+                </div>
+            </div>
+        );
+    }
+}
+
+GradientButton.propTypes = propTypes;
+
+
+export default GradientButton;
--- src/views/ShowEditor/components/assetCreator/index.js
+++ src/views/ShowEditor/components/assetCreator/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ClickableButton from './clickableButton';
+import GradientButton from './GradientButton';
 
 import styles from './style.css';
 
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 
 import * as assetsActions from 'services/editor/asset/actions';
 import * as slideActions from 'services/editor/slide/actions';
@@ -12,22 +12,6 @@ import * as assetTypes from 'services/editor/asset/assetTypes';
 import * as uiActions from 'services/ui/actions';
 
 class AssetCreator extends React.Component{
-
-  constructor(props){
-    super(props);
-
-      this.shapeTypes = [
-        {type : 'Circle', displayName : '원'},
-        {type : 'Rectangle', displayName : '사각형'},
-        {type : 'Triangle', displayName : '삼각형'}
-      ]
-
-       this.clickableItems = [
-         { icon : 'customAsset', onClick : this.openAssetStore},
-         { icon : 'slidePreview', onClick : this.showSlidePreview},
-         { icon : 'slideController', onClick : this.showSlideController}
-       ];
-  }
 
   render(){
     return (
@@ -43,31 +27,19 @@ class AssetCreator extends React.Component{
         </div>
       </div>
       <span className={styles.hr}/>
-      <ClickableButton name={'텍스트'} onClick={()=>this.createAsset(assetTypes.TYPE_TEXT)} />
-      <ClickableButton name={'이미지'} onClick={()=>this.createAsset(assetTypes.TYPE_IMAGE)} />
-      <ClickableButton name={'비디오'} onClick={()=>this.createAsset(assetTypes.TYPE_VIDEO)} />
-      <ClickableButton name={'도형'} onClick={()=>this.createAsset(assetTypes.TYPE_SHAPE)} />
-      <ClickableButton name={'표'} onClick={()=>this.createAsset(assetTypes.TYPE_TABLE)} />
-      <ClickableButton name={'기타'} onClick={()=>this.props.toggleAssetStore()} />
+      <GradientButton label={'텍스트'} onClick={()=>this.createAsset(assetTypes.TYPE_TEXT)} />
+      <GradientButton label={'이미지'} onClick={()=>this.createAsset(assetTypes.TYPE_IMAGE)} />
+      <GradientButton label={'비디오'} onClick={()=>this.createAsset(assetTypes.TYPE_VIDEO)} />
+      <GradientButton label={'도형'} onClick={()=>this.createAsset(assetTypes.TYPE_SHAPE)} />
+      <GradientButton label={'표'} onClick={()=>this.createAsset(assetTypes.TYPE_TABLE)} />
+      <GradientButton label={'기타'} onClick={()=>this.props.toggleAssetStore()} />
       <span className={styles.hr}/>
-      <ClickableButton name={'슬라이드 리스트'} onClick={()=>this.props.toggleSlideManager()} />
-      <ClickableButton name={'슬라이드 쇼'} onClick={()=>this.props.toggleSlideShow()} />
+      <GradientButton label={'슬라이드 리스트'} onClick={()=>this.props.toggleSlideManager()} />
+      <GradientButton label={'슬라이드 쇼'} onClick={()=>this.props.toggleSlideShow()} />
       </div>
       <div className={styles.logo}/>
       </div>
     );
-  }
-
-  showSlideController(){
-    console.log('test');
-  }
-
-  openAssetStore(){
-    console.log('test');
-  }
-
-  showDialog(subTypes){
-    console.log(subTypes.length);
   }
 
   createAsset(type){
