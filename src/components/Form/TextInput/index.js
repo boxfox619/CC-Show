11e18cfdac5abc1dd9e44617062ee2e797107e2c
Boxfox <boxfoxsg619@gmail.com>
2018-05-20 22:35:37 +0900

    Modify TextInput for expandable

--- src/components/Form/TextInput/index.js
+++ src/components/Form/TextInput/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import classnames from 'classnames';
 import styles from './style.css';
 import * as FormService from "../services/FormComponentService";
+import LabelText from '../LabelText';
 
 const propTypes = {
     onChange: React.PropTypes.func.isRequired,
     text: React.PropTypes.string.isRequired,
     className: React.PropTypes.string,
-    placeholder: React.PropTypes.string
+    placeholder: React.PropTypes.string,
+    label: React.PropTypes.string
 }
 
 const defaultProps = {
@@ -19,19 +21,33 @@ class TextInput extends React.Component {
 
     constructor(prop) {
         super(prop);
+        this.createCustomStyle = this.createCustomStyle.bind(this);
     };
 
     render() {
         return (
-          <input
-            type='text'
-            className={classnames(styles.text, this.props.className)}
-            style={FormService.createStyleObject(this.props)}
-            placeholder={this.props.placeholder}
-            onChange={(e) => this.props.onChange(e)}
-            value={this.props.text}
+          <div>
+            {!!this.props.label &&
+            <LabelText text={this.props.label} />
+                }
+            <input
+              type='text'
+              className={classnames(styles.text, this.props.className)}
+              style={this.createCustomStyle((FormService.createStyleObject(this.props)))}
+              placeholder={this.props.placeholder}
+              onChange={(e) => this.props.onChange(e.target.value)}
+              value={this.props.text}
           />
+          </div>
         );
+    }
+
+    createCustomStyle(style){
+        let tmpStyle = {...style};
+        if(!!tmpStyle.width){
+            tmpStyle['width'] = 'calc('+tmpStyle.width+' - 24px)';
+        }
+        return tmpStyle;
     }
 }
 
