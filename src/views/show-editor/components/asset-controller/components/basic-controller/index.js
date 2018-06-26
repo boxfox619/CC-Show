3d1cf6c4d76c240d9500ed4643fb1e722928218e
Boxfox <boxfoxsg619@gmail.com>
2018-06-27 00:53:15 +0900

    Implement style editor(remove json format & editing only key, value)

--- src/views/show-editor/components/asset-controller/components/basic-controller/index.js
+++ src/views/show-editor/components/asset-controller/components/basic-controller/index.js
@@ -53,6 +53,7 @@ class BasicController extends React.Component {
     this.setX_location = this.setX_location.bind(this);
     this.setY_location = this.setY_location.bind(this);
     this.setAngle = this.setAngle.bind(this);
+    this.getStyle = this.getStyle.bind(this);
     this.setStyle = this.setStyle.bind(this);
     this.setBorderWidth = this.setBorderWidth.bind(this);
     this.borderColorClickHandler = this.borderColorClickHandler.bind(this);
@@ -69,7 +70,7 @@ class BasicController extends React.Component {
                 <span className={styles.attribute_item_title}>H :</span>
                 <input className={styles.attribute_item_input}
                   onChange={this.setHeight}
-                  type="text"
+                  type={"text"}
                   value={this.props.height}
                 />
               </div>
@@ -77,7 +78,7 @@ class BasicController extends React.Component {
                 <span className={styles.attribute_item_title}>W :</span>
                 <input className={styles.attribute_item_input}
                   onChange={this.setWidth}
-                  type="text"
+                  type={"text"}
                   value={this.props.width}
                 />
               </div>
@@ -87,7 +88,7 @@ class BasicController extends React.Component {
                 <span className={styles.attribute_item_title}>X :</span>
                 <input className={styles.attribute_item_input}
                   onChange={this.setX_location}
-                  type="text"
+                  type={"text"}
                   value={this.props.x}
                 />
               </div>
@@ -95,7 +96,7 @@ class BasicController extends React.Component {
                 <span className={styles.attribute_item_title}>Y :</span>
                 <input className={styles.attribute_item_input}
                   onChange={this.setY_location}
-                  type="text"
+                  type={"text"}
                   value={this.props.y}
                 />
               </div>
@@ -104,7 +105,7 @@ class BasicController extends React.Component {
               <span className={styles.attribute_item_title}>A :</span>
               <input className={styles.attribute_item_input}
                 onChange={this.setAngle}
-                type="text"
+                type={"text"}
                 value={this.props.angle}
               />
             </div>
@@ -114,14 +115,14 @@ class BasicController extends React.Component {
         <ControllerWrapper title={'도형'}>
           <div className={styles.items}>
             <div className={styles.control_item}>
-              <span className={styles.attribute_item_title}><img src="/images/ic_color.png" /></span>
+              <span className={styles.attribute_item_title}><img src={"/images/ic_color.png"} /></span>
               <div className={styles.change_color}
                 onClick={this.backgroundColorClickHandler}
                 style={this.props.backgroundColor === 'white' ? {border: '1px solid #5D87B5'} : {backgroundColor: this.props.backgroundColor}}
               />
             </div>
             <div className={styles.control_item}>
-              <span className={styles.attribute_item_title}><img src="/images/ic_line.png" /></span>
+              <span className={styles.attribute_item_title}><img src={"/images/ic_line.png"} /></span>
               <div className={styles.attribute_both_input_area}>
                 <div className={styles.change_color}
                   onClick={this.borderColorClickHandler}
@@ -129,7 +130,7 @@ class BasicController extends React.Component {
                 />
                 <input className={styles.attribute_item_input}
                   onChange={this.setBorderWidth}
-                  type="text"
+                  type={"text"}
                   value={this.props.borderWidth}
                 />
               </div>
@@ -140,10 +141,9 @@ class BasicController extends React.Component {
         <ControllerWrapper title={'스타일'}>
           <div className={styles.items}>
             <div id={styles.input_style}>
-              <textarea cols=""
+              <textarea
                 onChange={this.setStyle}
-                rows=""
-                value={JSON.stringify(this.props.style, null, 4)}
+                defaultValue={this.getStyle()}
               />
             </div>
           </div>
@@ -172,9 +172,25 @@ class BasicController extends React.Component {
     this.props.onChangeAttribute('y', val);
   }
 
+  getStyle(){
+    let style = this.props.style;
+    let styleMap = '';
+    for(let key in style){
+      styleMap += key + ' : '+ style[key]+'\n';
+    }
+    return styleMap;
+  }
+
   setStyle(event) {
     let {value} = event.target;
-    this.props.onChangeAttribute('style', JSON.parse(value));
+    let style = {};
+    value.split("\n").forEach(line => {
+      if(line.includes(' : ')){
+        let lineVal = line.split(' : ');
+        style[lineVal[0]] = lineVal[1];
+      }
+    });
+    this.props.onChangeAttribute('style', style);
   }
 
   setAngle(event) {
