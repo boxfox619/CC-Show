25326cf0a38805286c1a1b8ffa86c8c276575839
Boxfox <boxfoxsg619@gmail.com>
2018-11-13 20:00:33 +0900

    Apply store provide & react-router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { browserHistory, Router } from 'react-router'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    static propTypes = {
+        store: PropTypes.object.isRequired,
+        routes: PropTypes.object.isRequired
+    };
+
+    render() {
+        return (
+            <Provider store={this.props.store}>
+                <div style={{height: '100%'}}>
+                    <Router history={browserHistory} children={this.props.routes}/>
+                </div>
+            </Provider>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store/store';
+import routes from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={store} routes={routes.default(store)}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
