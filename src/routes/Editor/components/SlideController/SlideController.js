1563b6eafc29d664953e23bba8f4fd61618b0296
Boxfox <boxfoxsg619@gmail.com>
2018-11-22 22:51:24 +0900

    Refactoring slide contoller & slide show dialog

---
+++ src/routes/Editor/components/SlideController/SlideController.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import GradientButton from 'components/form/button-gradient';
+
+import styles from './styles.css';
+import * as assetTypes from "constants/assetTypes";
+
+class SideController extends React.Component {
+    static propTypes = {
+        className: React.PropTypes.string.isRequired,
+        editorActions: React.PropTypes.object.isRequired,
+        uiActions: React.PropTypes.object.isRequired
+    };
+    render() {
+        return (
+            <div className={this.props.className}>
+                <div style={{'width': '80%', 'margin': '20px 10%'}}>
+                    <div className={styles.profile}>
+                        <div className={styles["profile-img-cover"]}>
+                            <img src={this.props.account.profile} />
+                        </div>
+                        <div className={styles["text-wrap"]}>
+                            <div className={styles["name"]}>{this.props.account.name}</div>
+                            <div className={styles["sub-name"]}>{this.props.account.email}</div>
+                        </div>
+                    </div>
+                    {renderGroup(this.props.editorActions, this.props.uiActions)}
+                </div>
+                <div className={styles.logo} />
+            </div>
+        );
+    }
+}
+
+const getButtonMap = (editorActions, uiActions) =>{
+    let actions = {...editorActions, ...uiActions};
+    return [
+        [
+            {label: '텍스트', action: () => actions.createAssetByType(assetTypes.TYPE_TEXT)},
+            {label: '이미지', action: () => actions.createAssetByType(assetTypes.TYPE_IMAGE)},
+            {label: '비디오', action: () => actions.createAssetByType(assetTypes.TYPE_VIDEO)},
+            {label: '도형', action: () => actions.createAssetByType(assetTypes.TYPE_SHAPE)},
+            {label: '표', action: () => actions.createAssetByType(assetTypes.TYPE_TABLE)},
+            {label: '기타', action: () => actions.toggleAssetStore()}
+        ],
+        [
+            {label: '슬라이드 리스트', action: () => actions.toggleSlideManager()},
+            {label: '슬라이드 쇼', action: () => actions.toggleSlideShow()}
+        ]
+    ];
+};
+
+export const renderGroup = (editorActions, uiActions) => {
+    let buttonMap = getButtonMap(editorActions, uiActions);
+    return buttonMap.map((group, idx) => {
+        return (
+            <div key={idx}>
+                <span className={styles.hr} />
+                {
+                    group.map((btn, btnIdx) => {
+                        return (
+                            <GradientButton
+                                key={idx + '-' + btnIdx}
+                                label={btn.label}
+                                onClick={btn.action}
+                            />)
+                    })
+                }
+            </div>);
+
+
+    });
+};
---
+++ src/routes/Editor/components/SlideShowDialog/SlideShowDialog.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import styles from './style.css';
+
+export default class SlideShowDialog extends React.Component {
+    static propTypes = {
+        slides: React.PropTypes.array.isRequired,
+        showId: React.PropTypes.string.isRequired,
+        setSlideNote: React.PropTypes.func.isRequired
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedSlide: 0
+        };
+    }
+
+    render() {
+        let showNote = this.props.slides[this.state.selectedSlide].note;
+        showNote = (showNote === undefined) ? '' : showNote;
+        return (
+            <div className={this.props.className}>
+                <header>
+                    <h1>SLIDE SHOW</h1>
+                </header>
+                <content className={styles["slide-show"]}>
+                    <div className={styles["slide-contents"]}>
+                        <div className={`${styles["slide-controller"]} ${styles["prev-slide"]}`} onClick={() => this.doSlide(-1)}>
+                            <img src="/images/ic_arrow_left_big.png"/>
+                        </div>
+
+                        <img className={styles['slide']} src={this.props.slides[this.state.selectedSlide].thumbnail}/>
+                        <div className={`${styles["slide-controller"]} ${styles["next-slide"]}`} onClick={() => this.doSlide(+1)}>
+                            <img src="/images/ic_arrow_right_big.png"/>
+                        </div>
+                        <div/>
+                    </div>
+                    <div className={styles["botton-container"]}>
+                        <div className={styles["show-note"]}>
+                          <textarea className={styles["content"]}
+                                    onChange={this.slideNoteChangeHandler}
+                                    placeholder="쇼 노트를 입력하세요"
+                                    value={showNote}
+                          />
+                        </div>
+                        <hr className={styles["split"]}/>
+                        <div className={styles["slide-number-wrapper"]}>
+                            <div className={styles["slide-number-context"]}>
+                                <img className={`${styles["slide-controller"]} ${styles["prev-slide"]}`}
+                                     onClick={() => this.doSlide(-1)}
+                                     src="/images/ic_arrow_left_small.png"
+                                />
+                                <div
+                                    className={styles['counter']}>{this.state.selectedSlide + 1}/{this.props.slides.length}</div>
+                                <img className={`${styles["slide-controller"]} ${styles["next-slide"]}`}
+                                     onClick={() => this.doSlide(+1)}
+                                     src="/images/ic_arrow_right_small.png"
+                                />
+                            </div>
+                            <div className={styles["slide-option-button"]}>
+                                <img onClick={() => window.open('/show/play/?show=' + this.props.showId, '_blank')}
+                                     src="/images/ic_fullscreen.png"
+                                />
+                                <img src="/images/ic_slide_show.png"/>
+                            </div>
+                        </div>
+                    </div>
+                </content>
+            </div>
+        )
+    }
+
+    slideNoteChangeHandler = (event) => {
+        this.props.setSlideNote(this.state.selectedSlide, event.target.value);
+    };
+
+    doSlide = (num) => {
+        let willSelectSlide = this.state.selectedSlide + num;
+        if (willSelectSlide < 0 || willSelectSlide === this.props.slides.length) {
+            return;
+        } else
+            this.setState({
+                selectedSlide: willSelectSlide
+            })
+    };
+}
