b43046c24b710ba047d91e8f0e89eee45333e349
boxfox619 <boxfoxsg619@gmail.com>
2018-11-26 22:42:15 +0900

    Refactoring Dialog component

--- src/core/components/Dialog/Progress/index.js
+++ src/core/components/Dialog/Progress/index.js
@@ -1,29 +1,29 @@
 import React, {Component} from 'react';
+import Dialog from '../Dialog';
 
 import styles from './style.css';
-import modalStyles from '../style.css';
 
 export default class ProgressDialog extends Component {
-  constructor(props){
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <div className={modalStyles.modal+' '+styles.customize}>
-        <div className={styles.container}>
-          <div className={styles.progress}>
-            <span />
-            <span />
-            <span />
-            <span />
-            <span />
-            <span />
-            <span />
-            <span />
-          </div>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <Dialog className={styles.customize}>
+                    <div className={styles.container}>
+                        <div className={styles.progress}>
+                            <span/>
+                            <span/>
+                            <span/>
+                            <span/>
+                            <span/>
+                            <span/>
+                            <span/>
+                            <span/>
+                        </div>
+                </div>
+            </Dialog>
+        )
+    }
 }
